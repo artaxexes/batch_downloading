7ae9d040d1d825bde1932c5e026552ebae3e9386
Artaxexes J. Ferreira <artaxexes@gmail.com>
2016-11-01 17:02:24 -0200

    regexp to match uri and filename from file, download and save files into specific folder

--- down-this-from.pl
+++ down-this-from.pl
@@ -4,4 +4,29 @@
 use 5.22.1;
 use strict;
 use warnings;
+use open qw< :encoding(UTF-8) >;
 use LWP::Simple;
+
+my $filename = "id.txt";
+my $handle = undef;
+
+open($handle, "<", $filename)  || die;
+
+my $img_regex = "([0-9]{4,6}\.jpg)";
+my @imgs = ();
+my $uri_regex = "(^https?://.*)";
+my $uri;
+
+while (<$handle>) {
+  if (m/^$img_regex$/) {
+    push @imgs, $1;
+  } elsif (m/^$uri_regex$/) {
+    $uri = $1;
+  }
+}
+
+for my $img (@imgs) {
+  my $path = "imgs/" . $img;
+  my $link = $uri . $img;
+  getstore($link, $path);
+}
