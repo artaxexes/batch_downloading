817d101008c8816f101da6bd5c1194dffff2bd01
Anddrei Ferreira <artaxexes@ymail.com>
2017-01-23 19:17:01 -0200

    script for check downloaded files

---
+++ check.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import json
+
+def last_dir(path):
+  dirs = []
+  for entry in os.scandir(path):
+    if entry.name.startswith('downloaded_') and entry.is_dir():
+      dirs.append(entry.name[11:len(entry.name)])
+  return max(dirs)
+
+def file_content(filename):
+  try:
+    f = open(filename)
+  except:
+    print('Could not open the file')
+  else:
+    handler = json.load(f)
+    f.close()
+  return handler['files']
+
+def compare(lines, files):
+  not_downloaded = []
+  for line in lines:
+    if line not in files:
+      not_downloaded.append(line)
+  return len(not_downloaded)
+
+if __name__ == '__main__':
+  lines = file_content('info.json')
+  files = os.listdir('downloaded_' + last_dir(os.getcwd()))
+  print(str(compare(lines, files)) + ' not downloaded')
